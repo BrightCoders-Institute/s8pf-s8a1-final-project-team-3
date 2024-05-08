bd545ad05d0595490fc84ab31cccf80184cbcda8
Jose Ocampo <jocampo3@ucol.mx>
2024-05-08 16:31:06 -0600

    Update file paths and import statements for consistency

--- BrightCommers/src/screens/Porfile.tsx
+++ BrightCommers/src/screens/Porfile.tsx
@@ -1,211 +1,143 @@
 import React from 'react';
-import { Text, TouchableOpacity, View } from 'react-native';
-import Logo from '../assets/icons/user.svg';
-import Flecha from '../assets/icons/arrowleft.svg';
-const Porfile = () => {
+import { Text, TouchableOpacity, View, Image } from 'react-native';
+import Edit from '../assets/icons/edit.svg';
+import LeftArrow from '../assets/icons/left.svg';
+
+const Profile = () => {
   return (
     <View style={styles.container}>
       <View style={styles.header}>
-      <View>
-          <Flecha style={styles.arrow}/>
-          </View>
-            <Text style={styles.textHeader}>Mi perfil </Text>
+        <LeftArrow />
+        <Text style={styles.title}>Mi Perfil</Text>
       </View>
-      <View style={styles.card}>
-      <View>
-           <Logo style={styles.logo}/> 
-      </View>
-          <Text style={styles.user}>Nombre de usuario</Text>
-          <Text style={styles.email}>ExampleCorreo@gmail.com</Text>
-      </View>
-      <View style={styles.cardEdit}>
 
-      <View>
-              <View>
-                <Text style={styles.userEdit}>Nombre de usuario:</Text>
-                <Text style={styles.userExample}>Nombre de usuario example</Text>
-             </View>
-              <View>
-                <Text style={styles.userEdit}>Correo:</Text>
-                <Text style={styles.userExample}>ExampleCorreo@gmail.com</Text>
-             </View>
-               <View>
-                <Text style={styles.userEdit}>Contrasena</Text>
-                <Text style={styles.userExample}>********</Text>
-             </View>
-           </View>
-             <View>
-              <View>
-               <TouchableOpacity style={styles.btnEditUser}>
-                <Text style={styles.txtEdit}>Editar</Text>
-               </TouchableOpacity>
-             </View>
-             <View>
-               <TouchableOpacity style={styles.btnEditEmail}>
-                <Text style={styles.txtEdit}>Editar</Text>
-               </TouchableOpacity>
-             </View>
-             <View>
-               <TouchableOpacity style={styles.btnEditPassword} >
-                <Text style={styles.txtEdit}>Editar</Text>
-               </TouchableOpacity>
-             </View>
-            </View>
-      </View>
-        {/* <View style={styles.header}>
-          <View>
-          <Flecha style={styles.arrow}/>
-          </View>
-            <Text style={styles.textHeader}>Mi perfil </Text>
-        </View>
-        <View style={styles.card}>
-          <View>
-           <Logo style={styles.logo}/> 
-          </View>
+      <View style={styles.card}>
+        <Image source={require('../assets/img/pfp.png')} style={styles.avatar}/>
+        <View style={styles.userInfoContainer}>
           <Text style={styles.user}>Nombre de usuario</Text>
           <Text style={styles.email}>ExampleCorreo@gmail.com</Text>
         </View>
-        <View style={styles.cardEdit}>
-            <View>
-              <View>
-                <Text style={styles.userEdit}>Nombre de usuario:</Text>
-                <Text style={styles.userExample}>Nombre de usuario example</Text>
-             </View>
-              <View>
-                <Text style={styles.userEdit}>Correo:</Text>
-                <Text style={styles.userExample}>ExampleCorreo@gmail.com</Text>
-             </View>
-               <View>
-                <Text style={styles.userEdit}>Contrasena</Text>
-                <Text style={styles.userExample}>********</Text>
-             </View>
-           </View>
-             <View>
-              <View>
-               <TouchableOpacity style={styles.btnEditUser}>
-                <Text style={styles.txtEdit}>Editar</Text>
-               </TouchableOpacity>
-             </View>
-             <View>
-               <TouchableOpacity style={styles.btnEditEmail}>
-                <Text style={styles.txtEdit}>Editar</Text>
-               </TouchableOpacity>
-             </View>
-             <View>
-               <TouchableOpacity style={styles.btnEditPassword} >
-                <Text style={styles.txtEdit}>Editar</Text>
-               </TouchableOpacity>
-             </View>
+      </View>
+
+      <View style={styles.cardEdit}>
+        <View>
+          <View style={styles.userInfoRow}>
+            <View style={styles.userInfo}>
+              <Text style={styles.user}>Nombre de usuario:</Text>
+              <Text style={styles.email}>Nombre de usuario example</Text>
             </View>
-        </View> */}
+            <TouchableOpacity style={styles.btnEditUser}>
+              <Edit />
+            </TouchableOpacity>
+          </View>
+          <View style={styles.userInfoRow}>
+            <View style={styles.userInfo}>
+              <Text style={styles.user}>Correo:</Text>
+              <Text style={styles.email}>ExampleCorreo@gmail.com</Text>
+            </View>
+            <TouchableOpacity style={styles.btnEditUser}>
+              <Edit />
+            </TouchableOpacity>
+          </View>
+          <View style={styles.userInfoLastRow}>
+            <View style={styles.userInfo}>
+              <Text style={styles.user}>Contraseña</Text>
+              <Text style={styles.email}>********</Text>
+            </View>
+            <TouchableOpacity style={styles.btnEditUser}>
+              <Edit />
+            </TouchableOpacity>
+          </View>
+        </View>
+      </View>
     </View>
   );
 };
 
 const styles = {
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',   
-        marginTop:10,
-    },
-    header: {
-        top: -36,
-        backgroundColor: '#1F379A',
-        width: '100%',
-        height: 66,
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-    arrow: {
-      right: 170,
-      top: 16,
-      width: 43,
-      height: 43,
-    },
-    textHeader:{
-      left: 1,
-      color: 'white',
-      fontSize: 24,  
-      top: -20
-    },
-    card: {
-        width: '92%',
-        height: 120,
-        top: -20,
-        backgroundColor: '#fff',
-        borderRadius: 10,
-        padding: 20,
-        margin: 10,
-        shadowColor: '#000',
-        shadowOffset: {
-          width: 0,
-          height: 2,
-        },
-        shadowOpacity: 0.25,
-        shadowRadius: 4,
-        elevation: 5,
-      },
-      logo: {
-        top: 13,
-        width: 74, 
-        height: 74,
-      },
-      user: {
-        left: 88,
-        fontSize: 19,
-        color: 'black',
-        fontWeight: 'bold',
-        top: -45,
-      },
-      email: {
-        left: 88,
-        fontSize: 16,
-        top: -43,
-      }, 
+  container: {
+    flex: 1,
+    padding: 16,
+    backgroundColor: 'white',
+  },
+  header: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: 16,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: '#282828',
+    paddingLeft: 12,
+    marginBottom: 2,
+  },
+  card: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingHorizontal: 12,
+    paddingVertical: 24,
+    borderRadius: 8,
+    marginBottom: 16,
+    borderColor: '#f5f5f5',
+    borderWidth: 2,
+  },
+  avatar: {
+    width: 64,
+    height: 64,
+    borderRadius: 50,
+    marginRight: 10,
+  },
+  userInfoContainer: {
+    flexDirection: 'column',
+  },
+  user:{
+    fontFamily: 'Inter',
+    fontSize: 16,
+    color: '#282828',
+    fontWeight: 'bold',
+  },
+  email:{
+    fontFamily: 'Inter',
+    fontSize: 14,
+    color: '#7E7E7E',
+  },
+  cardEdit: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between', 
+    paddingHorizontal: 16,
+    paddingVertical: 24,
+    borderRadius: 8,
+    marginBottom: 16,
+    borderColor: '#f5f5f5',
+    borderWidth: 2,
+  },
+  userInfoRow: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginBottom: 28,
+    width: '100%', 
+  },
+  userInfoLastRow: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    width: '100%', 
+  },
+  userInfo: {
+    flexDirection: 'column',
+  },
+  btnEditUser: {
+    backgroundColor: '#0139A6',
+    width: 28,
+    height: 28,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 50,
+  },
+};
 
-      cardEdit: {
-        flexDirection: 'row',
-        width: '92%',
-        height: 470,
-        top: -10,
-        backgroundColor: '#fff',
-        borderRadius: 10,
-        padding: 20,
-        margin: 10,
-        shadowColor: '#000',
-        shadowOffset: {
-          width: 0,
-          height: 2,
-        },
-        shadowOpacity: 0.25,
-        shadowRadius: 4,
-        elevation: 5,
-      },
-
-      userEdit:{
-        marginLeft:1,
-        marginTop: 30,
-        fontSize: 17,
-        color: 'black',
-        fontWeight: 'bold',
-      }, 
-      btnEditUser: {
-        marginTop: 42,
-        left: 80,
-      },
-      btnEditEmail: {
-        marginTop: 53,
-        left: 80,
-      },
-      btnEditPassword: {
-        marginTop: 55,
-        left: 80,
-      },
-      txtEdit: {
-        color:  '#1F379A',
-        textDecorationLine: 'underline',
-      }
-}
-
-export default Porfile
+export default Profile;
